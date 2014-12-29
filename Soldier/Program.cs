741057dfa4fd03b98f3c6f12b1892fb60004fb40
L. David Varvel <dvarvel@pivotal.io>
2014-12-29 15:08:15 -0500

    WIP exploratory soldier using MS deploy (broke tests)

--- Soldier/Program.cs
+++ Soldier/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Web.Administration;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -13,22 +15,26 @@ namespace Soldier
     {
         static void Main(string[] args)
         {
-            var thingToRun = args[0];
-            var workingDir = args[1];
-            var command = thingToRun.Split(' ');
+            var containerRootPath = System.IO.Directory.GetCurrentDirectory();
+            var containerID = new DirectoryInfo(containerRootPath).Name;
+            //HARDCODE LOCATION OF ZIPFILE
+            var zipFileLocation = containerRootPath + "\\app\\webdeploy\\Nora.zip";
+
+            //CRAZY MSDEPLOY COMMAND LINE.  THIS SHOULD BE MADE BETTERER.
+            var deployCommand = "\"C:\\Program Files\\IIS\\Microsoft Web Deploy V3\\msdeploy.exe\" -verb:sync -source:package="+zipFileLocation+" -dest:auto -setParam:name='IIS Web Application Name',value=\"" + containerID + "\" -presync:runCommand='\"C:\\Windows\\System32\\inetsrv\\appcmd set site " + containerID + " /bindings:http/*:8080:'";
+
             ProcessStartInfo startInfo;
-            if (command.Length > 1)
-            {
-                startInfo = new ProcessStartInfo(command[0], command[1]);
-            }
-            else
-            {
-                startInfo = new ProcessStartInfo(command[0]);
-            }
-            startInfo.WorkingDirectory = workingDir;
+            
+            startInfo = new ProcessStartInfo(deployCommand);
+            
+            startInfo.WorkingDirectory = containerRootPath;
             var process = new Process {StartInfo = startInfo};
             process.Start();
             process.WaitForExit();
+
+            ServerManager serverManager = ServerManager.OpenRemote("localhost");
+            Site site = serverManager.Sites[containerID];
+            site.Start();
         }
     }
 }
