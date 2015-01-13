96e10d1e3405ef22744d4d923f28383b7583f791
David Tengdin & Brannon Jones <pairs+dtengdin+brannon@pivotal.io>
2015-01-13 11:52:33 -0500

    Revert "remove copying hard coded web config [#85274818]"
    This reverts commit b0ade209285ec0f81abb291df7e4b23d3517b164.
    
    Conflicts:
    	Soldier/Program.cs
    	windows-circus.tgz

--- Soldier/Program.cs
+++ Soldier/Program.cs
@@ -34,6 +34,9 @@ namespace Soldier
             process.Start();
             process.WaitForExit();
 
+            var stockWebConfig = Path.Combine(containerRootPath, "tmp", "circus", "Web.config");
+            var webConfigDestination = Path.Combine(containerRootPath, "Web.config");
+            File.Copy(stockWebConfig, webConfigDestination);
             ServerManager serverManager = ServerManager.OpenRemote("localhost");
             Site site = serverManager.Sites[containerID];
             site.Start();
