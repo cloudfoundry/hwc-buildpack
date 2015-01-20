939ce3479082a92299f10db3b4abaed8909573a8
mavenraven.org <developers@mavenraven.org>
2015-01-20 17:00:08 -0500

    update the Tailor to point the app to the root rather than '/'

--- Tailor/Program.cs
+++ Tailor/Program.cs
@@ -48,7 +48,8 @@ namespace Tailor
             JObject execution_metadata = new JObject();
             execution_metadata["start_command"] = "tmp/Circus/WebAppServer.exe";
             execution_metadata["start_command_args"] = new JArray()
-            { new JValue("8080"), new JValue("/app"), };
+            { new JValue("8080"), new JValue("/"), };
+
             JObject detected_start_command = new JObject();
             detected_start_command["web"] = "the start command";
 
