08e802ab1eab475eddf48d1d31015cda25065895
Dave Goddard <dgoddard@pivotal.io>
2015-02-12 21:15:40 +0000

    Healthcheck reports the port that it would check (debugging)

--- Healthcheck/Program.cs
+++ Healthcheck/Program.cs
@@ -10,6 +10,7 @@ namespace Healthcheck
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("If I was to check a port, it would be: {0}", Environment.GetEnvironmentVariable("PORT"));
             System.Console.WriteLine("Hi I am dummy Healthcheck, please replace.");
         }
     }
