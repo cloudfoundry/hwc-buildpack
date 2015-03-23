0cc1aae4371dda8b2b0669a206b7e72c167d6e0d
Dave goddard & Ben Moss <pair+dgoddard+bmoss@pivotal.io>
2015-03-23 10:22:26 -0400

    Delete using rmdir

--- make.bat
+++ make.bat
@@ -1,8 +1,10 @@
 SET BIN=%CD%\bin
 SET PATH=%BIN%;%PATH%
 
-del /F /Q packages\* windows_app_lifecycle.tgz
+del /F /Q windows_app_lifecycle.tgz
+rmdir /S /Q packages
 nuget restore || exit /b 1
+
 devenv WindowsCircus.sln /build "Release" || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Release\BuilderTests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Release\LauncherTests.dll || exit /b 1
