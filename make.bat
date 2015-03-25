49df6a3a17b9bbb00a229779f60dd7e05f15364a
John Shahid & David Morhovich <pair+jshahid+dmorhovich@pivotal.io>
2015-03-25 10:13:24 -0400

    add %WINDIR%\SysNative to the path (to get dism) and fix the delete

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 :: msbuild must be in path
-SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319
+SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319;%WINDIR%\SysNative
 where msbuild
 if errorLevel 1 ( echo "msbuild was not found on PATH" && exit /b 1 )
 
@@ -9,7 +9,7 @@ dism /online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart
 dism /online /Enable-Feature /FeatureName:Application-Server-WebServer-Support /FeatureName:AS-NET-Framework /All /NoRestart
 dism /online /Enable-Feature /FeatureName:IIS-HostableWebCore /All /NoRestart 
  
-del /F /Q windows_app_lifecycle.tgz
+del /F /Q windows_app_lifecycle*.tgz
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
 
