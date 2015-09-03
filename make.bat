ec356896d4c89a2d9df85377a57c1de148fe9897
John Shahid <jshahid@pivotal.io>
2015-09-03 16:33:20 -0400

    compiles and tar the diego ssh daemon
    [#97325528]
    
    Signed-off-by: John Shahid <jshahid@pivotal.io>

--- make.bat
+++ make.bat
@@ -13,13 +13,21 @@ del /F /Q windows_app_lifecycle*.tgz
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
 
+SET GOPATH=%CD%\..\..\..\..
+SET GOBIN=%GOPATH%\bin
+pushd %GOPATH%\src\github.com\cloudfoundry-incubator\diego-ssh
+      go get github.com/onsi/ginkgo/ginkgo
+      go get -t ./...
+      ginkgo -r -noColor . || exit /b 1
+popd
+go build -o diego-sshd.exe github.com/cloudfoundry-incubator/diego-ssh/cmd/sshd || exit /b 1
+
 MSBuild WindowsAppLifecycle.sln /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Healthcheck.Tests\bin\Release\Healthcheck.Tests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Release\BuilderTests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Release\LauncherTests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe WebAppServer.Tests\bin\Release\WebAppServer.Tests.dll || exit /b 1
-bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\Healthcheck\bin . -C ..\..\WebAppServer\bin . || exit /b 1
-bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\Healthcheck\bin . -C ..\..\WebAppServer\bin . || exit /b 1
+bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\Healthcheck\bin . -C ..\..\WebAppServer\bin . -C ..\.. diego-sshd.exe || exit /b 1
 for /f "tokens=*" %%a in ('git rev-parse --short HEAD') do (
     set VAR=%%a
     )
