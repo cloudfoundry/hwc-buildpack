68990b2ddacfa38a9b5cb127efa36021d864907c
Stefan Schneider <stefan.schneider@hpe.com>
2016-02-17 08:42:54 -0800

    Buildpack extraction WIP

---
+++ Base/Models/Models.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Base.Models
+{
+    public class Service
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("label")]
+        public string Label { get; set; }
+        [JsonProperty("tags")]
+        public List<string> Tags { get; set; }
+        [JsonProperty("credentials")]
+        public IDictionary<string, string> Credentials { get; set; }
+    }
+
+
+    public class Services
+    {
+        public Services()
+        {
+            UserProvided = new List<Service>();
+        }
+
+        [JsonProperty("user-provided")]
+        public List<Service> UserProvided { get; set; }
+    }
+}
---
+++ Compile/Program.cs
@@ -0,0 +1,41 @@
+﻿using Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Invalid usage. Expected: compile.exe <build_path> <cache_path>");
+                Environment.Exit(1);
+            }
+
+            var buildPath = args[0];
+            var cachePath = args[1];
+
+            var binDirectory = new FileInfo(System.Reflection.Assembly.GetEntryAssembly().Location).Directory;
+
+            var files = Directory.EnumerateFiles(buildPath).ToList();
+            if (Utils.HasWebConfig(files))
+            {
+                Directory.CreateDirectory(Path.Combine(buildPath, "WebAppServer", "bin"));
+                Utils.CopyDirectory(Path.Combine(binDirectory.Parent.FullName, "WebAppServer", "bin"), Path.Combine(buildPath, "WebAppServer"));
+                Utils.SetConnectionStrings(files);
+            }
+            if (Utils.ExeFiles(files).Count() == 1)
+            {
+                // Nothing to do.
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
