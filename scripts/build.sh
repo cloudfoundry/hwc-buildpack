8f602b62fa9350dba46281c8010bb0fab7ecd826
sunjayBhatia <sbhatia@pivotal.io>
2017-01-31 21:15:23 -0500

    build script produced a zip with absolute paths

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-BINDIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )/bin"
+ROOTDIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
+BINDIR=$ROOTDIR/bin
 
 set -ex
 
@@ -9,4 +10,6 @@ GOOS=windows go build -o $BINDIR/detect.exe github.com/greenhouse-org/hwc-buildp
 GOOS=windows go build -o $BINDIR/compile.exe github.com/greenhouse-org/hwc-buildpack/compile
 GOOS=windows go build -o $BINDIR/release.exe github.com/greenhouse-org/hwc-buildpack/release
 
-zip -r hwc_buildpack.zip $BINDIR
+pushd $ROOTDIR &> /dev/null
+zip -r hwc_buildpack.zip bin
+popd &> /dev/null
