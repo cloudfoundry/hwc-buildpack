88c6f3a62ba91cabe1d4a2d0f3baf79d89b62e71
Tyler Phelan <tphelan@pivotal.io>
2018-10-30 14:30:34 -0400

    Set GOBIN as .bin
    Go get ginkgo
    
    Co-authored-by: Zander Mackie <amackie@pivotal.io>

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -5,10 +5,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
 if [ ! -f .bin/ginkgo ]; then
-  go install github.com/onsi/ginkgo
-# (cd src/*/vendor/github.com/onsi/ginkgo/ginkgo/ && go install)
+  go get -u github.com/onsi/ginkgo/ginkgo
 fi
 if [ ! -f .bin/buildpack-packager ]; then
   go install github.com/cloudfoundry/libbuildpack/packager/buildpack-packager
-# (cd src/*/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager && go install)
 fi
